2de1f91841e7614d8328e00bed2592c0644cadda
gabriel perninha <gabrielperninha.carvalho@gmail.com>
2025-08-04 21:19:33 -0300

    at3: click e lazy column

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -43,13 +46,46 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    val artist = Artist(
+                    val leonardo = Artist(
                         name = "Leonardo da Vinci",
                         lastSeeOnline = "3 minutes ago",
                         image = R.drawable.ic_leonardo_da_vinci,
                         art = R.drawable.ic_mona_lisa
                     )
-                    ArtistCard(artist)
+
+                    val picasso = Artist(
+                        name = "Pablo Picasso",
+                        lastSeeOnline = "5 minutes ago",
+                        image = R.drawable.ic_pablo_picasso,
+                        art = R.drawable.ic_beijo
+                    )
+
+                    val vanGogh = Artist(
+                        name = "Vicent Van Gogh",
+                        lastSeeOnline = "2 minutes ago",
+                        image = R.drawable.ic_vincent_van_gogh,
+                        art = R.drawable.ic_starry_night
+                    )
+
+                    val salvador = Artist(
+                        name = "Salvador Dali",
+                        lastSeeOnline = "7 minutes ago",
+                        image = R.drawable.ic_salvador_dali,
+                        art = R.drawable.ic_persistence_of_memory
+                    )
+
+                    val artists = listOf(vanGogh, salvador, picasso, leonardo)
+
+                    LazyColumn {
+                        items(artists) { artist ->
+                            ArtistCard(
+                                artist = artist,
+                                onClick = {
+                                    println("testando click")
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -58,10 +94,16 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ArtistCard(artist: Artist) {
+fun ArtistCard(
+    artist: Artist,
+    onClick: () -> Unit
+
+) {
 
     Column(
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
     ) {
 
         Row(verticalAlignment = Alignment.CenterVertically) {
@@ -121,7 +163,12 @@ fun ArtistCardPreview() {
             image = R.drawable.ic_leonardo_da_vinci,
             art = R.drawable.ic_mona_lisa
         )
-        ArtistCard(artist)
+        ArtistCard(
+            artist = artist,
+            onClick = {
+                println("testando click")
+            }
+        )
     }
 }
 
